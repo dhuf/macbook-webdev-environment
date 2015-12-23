2859ebef5334c5f42974db7a8343a0cdf4cfddec
Jonas Renggli <jonas.renggli@swisscom.com>
2015-12-23 19:52:52 +0100

    [WORKINPROGRESS]

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# Install Homebrew
+#
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo chown root /usr/local/bin/brew
+
+
+#
+# Install Ansible
+#
+sudo brew install ansible
+
+
+#
+# Prepare Ansible Playbooks
+#
+sudo ansible-galaxy install -r ansible-playbooks.requirements.txt --force
+
+
+#
+# Run Playbook
+#
+sudo ansible-playbook main.yml -i inventory
